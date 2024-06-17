c56bd5ca7101e9bd27cc4c850413f2775a9d0d57
unknown <divya.merlin444@gmail.com>
2024-06-18 00:13:06 +0530

    conflict1

--- src/com/sample/ForGit.java
+++ src/com/sample/ForGit.java
@@ -4,7 +4,7 @@ public class ForGit {
 
 	public static void main(String[] args) {
 		System.out.println("For Git testing purpose");
-		System.out.println("edited");
+		System.out.println("edited to test conflict");
 	}
 
 }
